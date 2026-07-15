fc1b196e8c5537530969e079190aec74a7980640
Shivani Parasar <144723212+shivasar@users.noreply.github.com>
2026-07-15 08:38:59 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       if(s.length()!=t.length()) return false;
+       map<char,int>mp;
+       for(auto it: s){
+        mp[it]++;
+       } 
+       for(char it: t){
+        mp[it]--;
+       }
+       for(auto it: mp){
+        if(it.second<0) return false;
+       }
+       return true;
+    }
+};
